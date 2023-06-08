feb9e0fc471057dd76dc0a2b31ecd7a4c7a9140f
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-06-08 12:35:21 +0200

    :sparkles: Heritage

--- src/pizzas/entities/calzone.entity.ts
+++ src/pizzas/entities/calzone.entity.ts
@@ -1,20 +1,20 @@
 import { Column, Entity, Index } from 'typeorm'
 import { Recipe } from './recipe.entity'
 
-export enum PizzaFlavor {
+export enum Calzonelavor {
   Tomatoes = 'T',
   Cream = 'C'
 }
 
 @Entity()
-export class Pizza extends Recipe {
+export class Calzone extends Recipe {
   @Index({ unique: true })
   @Column({ length: 100 })
   name!: string
 
   @Index()
-  @Column({ type: 'enum', enum: PizzaFlavor })
-  flavor!: PizzaFlavor
+  @Column({ type: 'enum', enum: Calzonelavor })
+  flavor!: Calzonelavor
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   created!: Date
--- src/pizzas/pizzas.module.ts
+++ src/pizzas/pizzas.module.ts
@@ -3,9 +3,11 @@ import { PizzasService } from './pizzas.service'
 import { PizzasController } from './pizzas.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Pizza } from './entities/pizza.entity'
+import { Calzone } from './entities/calzone.entity'
+import { Recipe } from './entities/recipe.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pizza])],
+  imports: [TypeOrmModule.forFeature([Pizza, Calzone, Recipe])],
   controllers: [PizzasController],
   providers: [PizzasService]
 })
